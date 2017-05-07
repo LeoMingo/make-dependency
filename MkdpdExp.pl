f05849ec01bcd7984ba31b0d2d8889a615d7bfc0
root <LeoMingo>
2017-05-07 18:56:06 +0800

    Add some widget functions

--- MkdpdExp.pl
+++ MkdpdExp.pl
@@ -1,6 +1,6 @@
 use warnings;
 use strict;
-use diagnostics;
+#use diagnostics;
 
 use feature "say";
 
@@ -30,12 +30,61 @@ draw_dpd_chart
 
 =cut
 
+sub first_char {
+    my $str = shift;
+    my @str_arr = split //, $str;
+    return $str_arr[0];
+}
+
+sub last_char {
+    my $str = shift;
+    my @str_arr = split //, $str;
+    return $str_arr[$#str_arr];
+}
+
+sub concat_arr {
+    my $arr_ref1 = $_[0];
+    my $arr_ref2 = $_[1];
+    my @arr1 = @$arr_ref1;
+    my @arr2 = @$arr_ref2;
+
+    foreach my $i (@arr2) {
+        push @arr1, $i;
+    }
+    return @arr1;
+}
+
+
+sub rm_elem {
+    my $arr_ref = $_[0];
+    my $idx = $_[1];
+    
+    my @arr = @$arr_ref;
+    my @leftpart = ();
+    my @rightpart = ();
+
+    for(my $i = 0; $i < $idx; $i++){
+        push @leftpart, $arr[$i],
+    }
+    for(my $i = $idx+1; $i <= $#arr; $i++){
+        push @rightpart, $arr[$i],
+    }
+
+    say @leftpart, @rightpart;
+    say "abc";
+
+    my @new_arr = concat_arr(\@leftpart, \@rightpart);
+    return @new_arr;
+}
+
+
 
 
 sub make_directory {
     my $dir_name = shift;
     mkdir($dir_name, 0755);
 }
+
 
 
 
@@ -68,18 +117,124 @@ sub write_string {
 my @dir_dpd_arr = ();
 my @file_dpd_arr = ();
 
-#(level-option, str_behavior)
+#Direct access unit
+#a leveloption-stringbehavior array
+#(level-option, string-behavior)
+#e.g. ("+1", "folder/")
 
 
-sub levelOp_duplicate {
-    my @pair1 = $_[0];
-    my @pair2 = $_[1];
-    my $
+
+
+sub make_dpd_line_arr {
+    my $dpd_line = shift;
+    my @dpd_line_char_arr = split //, $dpd_line;
+    my @levelOp_char_arr = ();
+    my @str_behav_char_arr = ();
+    
+    my @dpd_line_arr = ();
+
+    for my $i (0..1) {
+        push $dpd_line_char_arr[$i], @levelOp_char_arr;
+    }
+    for(my $i = 2; $i < $#dpd_line_char_arr; $i++){
+        push $dpd_line_char_arr[$i], @str_behav_char_arr;
+    }
+    
+    my $levelOp = join //, @levelOp_char_arr;
+    my $str_behav = join //, @str_behav_char_arr;
+    
+    push $levelOp @dpd_line_arr;
+    push $str_behav @dpd_line_arr;
+
+    return @dpd_line_arr;
 }
-sub rm_elem {
-        
+
+
+sub levelOp {
+    my @dpdlarr = @_;
+    return $dpdlarr[0];
 }
 
+sub dpdl_str_behav {
+    my @dpdlarr = @_;
+    return $dpdlarr[1];
+}
+
+
+
+######Level-Option manipulation###########
+
+sub make_levelOp_arr {
+    my @dpdlarr = @_;
+    my $levelOp = levelOp @dpdlarr;
+    my @levelOp_arr = split //, $levelOp;
+    return @levelOp_arr;
+}
+
+
+sub dpdl_level {
+    my @dpdlarr = @_;
+    my @levelOp_arr = make_levelOp_arr @dpdlarr;
+    my $level = (levelOp_arr)[1];
+    return $level;
+}
+
+
+sub dpdl_op {
+    my @dpdlarr = @_;
+    my @levelOp_arr = make_levelOp_arr @dpdlarr
+    my $option = (@levelOp_arr)[0];
+    return $option;
+}
+
+
+sub same_level {
+    my $dpdlarr_ref1 = $_[0];
+    my $dpdlarr_ref2 = $_[1];
+    my $level1 = dpdl_level @$dpdlarr_ref1;
+    my $level2 = dpdl_level @$dpdlarr_ref2;
+
+    if($level1 eq $level2){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+sub dpdl_datatype {
+    my @dpdlarr = @_
+    my $sb = str_behav @dpdlarr;
+
+    if ((first_char $sb eq '"') && (last_char $sb eq '"')){
+        return "s";
+    }elsif (last_char $sb eq '/'){
+        return "d";
+    }else {
+        return "f";
+    }
+}
+
+sub same_dpdl_datatype {
+    my $dpdlarr_ref1 = $_[0];
+    my $dpdlarr_ref2 = $_[1];
+    my $datatype1 = dpdl_datatype @$dpdlarr_ref1;
+    my $datatype2 = dpdl_datatype @$dpdlarr_ref2;
+
+    if ($datatype1 eq $datatype2){
+        return 1;
+    }else {
+        return 0;
+    }
+}
+
+
+
+###############String-Behavior Manipulation###################
+
+##############################################################
+
+
+=head1
 
 sub push_to_dda {
     my @args = @_;
@@ -99,7 +254,7 @@ sub push_to_dda {
 
 
 
-=head1
+#=head1
 sub push_to_fda {
     my @args = @_;
     my $level_op = $args[0];
